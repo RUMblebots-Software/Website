dfd1575118199ab6624e43b6039d7614e5ad828d
JuanQ1226 <juan.quintana5@upr.edu>
2023-01-19 17:38:32 -0400

    Started the setup of the firebase database

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const config = require("./webpack.config")
 const compiler = webpack(config)
 import { initializeApp } from "firebase/app"
 import { getAnalytics } from "firebase/analytics"
+import { getDatabase } from "firebase/database";
 //Web APP firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyDHjvrenveN0OBqC167mRxsmxWN7D4M6Wc",
@@ -12,10 +13,12 @@ const firebaseConfig = {
   storageBucket: "rumblebots-website.appspot.com",
   messagingSenderId: "445812151859",
   appId: "1:445812151859:web:b409a58e9f7eb869c57546",
-  measurementId: "G-D8XD7NLTM7"
+  measurementId: "G-D8XD7NLTM7",
+  databaseURL:"https://rumblebots-website-default-rtdb.firebaseio.com/"
 };
 
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 const analytics = getAnalytics(app);
 
 
